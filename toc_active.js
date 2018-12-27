63dd0fc7b831e1b4d4fc534f063238911cd7cb15
lwx5924 <lwx5924@gmail.com>
2018-12-27 14:14:22 +0800

    * --amend

---
+++ toc_active.js
@@ -0,0 +1,11 @@
+
+
+$(document).ready(function(){
+
+   $("div").mouseenter(function(){
+   	 var id = $(this).attr('id');
+   	 $('a').removeClass('active');
+   	 $("[href=#"+id+"]").addClass('active');
+   });
+
+});
